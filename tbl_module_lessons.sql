4b92fc3fe3abab753d22c24b67c430dfef91c956
ZethPierce <56748697+ZethPierce@users.noreply.github.com>
2020-07-07 02:11:30 +0800

    Add files via upload

---
+++ tbl_module_lessons.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 06, 2020 at 08:08 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `imelt_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_module_lessons`
+--
+
+CREATE TABLE `tbl_module_lessons` (
+  `general_id` int(11) NOT NULL,
+  `module_id` int(5) NOT NULL,
+  `lesson_contents` varchar(80) NOT NULL,
+  `lesson_figures` varchar(50) NOT NULL,
+  `lesson_images` varchar(100) NOT NULL,
+  `lesson_texts` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
